7a503e692ae45ba9d0e933ef5d516b6c48faabc6
Oli <olifer97@gmail.com>
2020-04-26 15:21:08 -0300

    Add POST to create a user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
 
     def index
       render json: User.all, each_serializer: UserSerializer
@@ -6,5 +7,14 @@ class UsersController < ApplicationController
 
     def show
       render json: User.find(params[:id])
+    end
+
+    def create
+      @user = User.create(user_params)
+      render json: @user.to_json
+    end
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email)
     end
   end
