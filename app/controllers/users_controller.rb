63d4efad3193896a43b71b42b79ca1c8790961b8
Oli <olifer97@gmail.com>
2020-04-28 17:05:53 -0300

    Run rubocop linter.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,22 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
   skip_before_action :verify_authenticity_token
 
-    def index
-      render json: User.all, each_serializer: UserSerializer
-    end
-
-    def show
-      render json: User.find(params[:id])
-    end
-
-    def create
-      @user = User.create(user_params)
-      render json: @user.to_json
-    end
-
-    def user_params
-      params.require(:user).permit(:first_name, :last_name, :email)
-    end
+  def index
+    render json: User.all, each_serializer: UserSerializer
   end
+
+  def show
+    render json: User.find(params[:id])
+  end
+
+  def create
+    @user = User.create(user_params)
+    render json: @user.to_json
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email)
+  end
+end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class UserSerializer < ActiveModel::Serializer
-    attributes :first_name, :last_name, :email
-  end
+  attributes :first_name, :last_name, :email
+end
