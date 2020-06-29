628df35b28fc6e6092974500a6373848313e9189
chortas <chortas@fi.uba.ar>
2020-06-29 18:08:50 -0300

    Added request with param

--- app/controllers/monitoring.py
+++ app/controllers/monitoring.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, request, jsonify
 from flask import current_app as app
 from database.models.user_stat import UserStat
+from datetime import datetime
 
 bp_monitor = Blueprint("bp_monitor", __name__)
+
+TIME_FORMAT = "%m/%d/%y %H:%M:%S"
 
 # -- Endpoints
 
@@ -12,10 +15,15 @@ def ping():
 
 @bp_monitor.route('/stats')
 def stats():
+    body = request.get_json()
+    date = body['timestamp']
+    date_to_timestamp = datetime.strptime(date, TIME_FORMAT)
     response = []
     user_stats = UserStat.objects
     for stat in user_stats:
-        response.append({"num_users": stat.num_users, "timestamp": stat.timestamp})
+        act_timestamp = datetime.strptime(stat.timestamp, TIME_FORMAT)
+        if (date_to_timestamp - act_timestamp).total_seconds() < 0: 
+            response.append({"num_users": stat.num_users, "timestamp": stat.timestamp})
     request_response = jsonify(response)
     request_response.status_code = 200
     return request_response
